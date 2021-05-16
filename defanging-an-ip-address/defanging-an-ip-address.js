08318b7e7e38bb4538520ee99a31b3e8ffb8d812
DianaTs1 <73934231+DianaTs1@users.noreply.github.com>
2021-05-16 00:31:57 -0400

    Time: 116 ms (5.27%), Space: 38.2 MB (85.35%) - LeetHub

--- defanging-an-ip-address/defanging-an-ip-address.js
+++ defanging-an-ip-address/defanging-an-ip-address.js
@@ -2,15 +2,7 @@
  * @param {string} address
  * @return {string}
  */
-var defangIPaddr = function(address) {
-    function replaceAll(string, search, replace) {
-  return string.split(search).join(replace);
-}
-     const search = '.';
-const replaceWith = '[.]';
-
-const result = replaceAll(address, search, replaceWith);
-
-return result
+var defangIPaddr = function(address) {  
+  return address.split(".").join("[.]");
 
 };
