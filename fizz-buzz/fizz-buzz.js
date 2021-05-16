7e1bae6dc701a2f967e4bb90b924f7817aca27a2
DianaTs1 <73934231+DianaTs1@users.noreply.github.com>
2021-05-15 20:11:19 -0400

    Time: 176 ms (6.97%), Space: 47.6 MB (5.30%) - LeetHub

---
+++ fizz-buzz/fizz-buzz.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    let final_array = []
+    for (let i=1; i<n+1; i++){
+        if (i % 15 === 0){
+            final_array.push("FizzBuzz")
+            }
+        else if (i % 5 === 0){
+            final_array.push("Buzz")
+        }
+        else if (i % 3 === 0){
+            final_array.push("Fizz")
+        }
+        else {
+            num = i.toString()
+            final_array.push(num)
+        }
+        console.log(i)
+    }
+    return final_array
+};
