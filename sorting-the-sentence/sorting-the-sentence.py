28c0fd34fbdd514ddf85ae4eb23fda953ad9cede
DianaTs1 <73934231+DianaTs1@users.noreply.github.com>
2021-06-01 17:22:15 -0400

    shorten the code

--- sorting-the-sentence/sorting-the-sentence.py
+++ sorting-the-sentence/sorting-the-sentence.py
@@ -3,23 +3,19 @@ class Solution:
         
         temp_list = s.split(" ")
         temp_dict = {}
-        list_of_nones = []
 
         for elem in temp_list:
-            list_of_nones.append(None)
-
-        for elem in temp_list:
-            index = int(elem[int(len(elem) - 1)]) - 1
-            temp_dict[index] = index, elem[:int(len(elem) - 1)]
+            index = elem[-1]
+            temp_dict[index] = index, elem[:-1]
 
         sorted_list = sorted(temp_dict.values())
 
-        final_string = sorted_list[0][1]
-
-        for elem in range(1, len(sorted_list)):
-            final_string += " " + sorted_list[elem][1]
+        final_string = ""
 
 
-        return final_string
+        for index, word in sorted_list:
+            final_string += " " + word
+
+        return final_string.strip()
         
         
