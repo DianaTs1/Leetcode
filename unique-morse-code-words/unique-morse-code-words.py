eeb2eb899dfb73a9ad0aa54533b908b6b44e0617
DianaTs1 <73934231+DianaTs1@users.noreply.github.com>
2021-06-13 18:00:50 -0400

    added comments

--- unique-morse-code-words/unique-morse-code-words.py
+++ unique-morse-code-words/unique-morse-code-words.py
@@ -1,16 +1,17 @@
 import string
 
-
 class Solution:
     def uniqueMorseRepresentations(self, words: List[str]) -> int:
         english_alphabet = list(string.ascii_lowercase)
-        morse_Code = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        morse_code = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
         
+        ## Create a dictionary with the keys of english letters and value of morse codes
         alphabet_dictionary = {}
         
         for letter in range(len(english_alphabet)):
-            alphabet_dictionary[english_alphabet[letter]] = morse_Code[letter]
+            alphabet_dictionary[english_alphabet[letter]] = morse_code[letter]
         
+        ## Create a list that only contains unique transformations  of words
         temp_list = []
         for word in words:
             temp_morse_code = ""
