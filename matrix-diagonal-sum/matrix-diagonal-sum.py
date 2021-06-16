c987ab0aef2d25cd8b017343d55e9b4e35889d55
DianaTs1 <73934231+DianaTs1@users.noreply.github.com>
2021-06-15 20:43:53 -0400

    shorter code

--- matrix-diagonal-sum/matrix-diagonal-sum.py
+++ matrix-diagonal-sum/matrix-diagonal-sum.py
@@ -6,13 +6,9 @@ class Solution:
         ## add elments of the primary and secondary diagolans
         for i in range(len(mat)):
             sum += mat[i][i]
-            sum += mat[i][length]
-            length -= 1
+            sum += mat[i][length-i]
         
         ##if the lenght of mat is odd, subtract the middle number that has been added twice
         if len(mat) % 2 == 1:
-            length = len(mat)-1
             sum = sum - mat[length//2][length//2]
-            return sum 
-        else:
-            return sum
+        return sum
