3565b403f3e42fa07318f7c9979f52b353d87125
Diana Tsertsvadze <diianadesign@gmail.com>
2021-09-01 22:37:06 -0400

    Time: 40 ms (13.09%), Space: 14.1 MB (72.99%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.py
@@ -0,0 +1,18 @@
+class Solution:
+    
+    def __init__(self):
+        
+        self.cache = {}
+    
+    def climbStairs(self, n: int) -> int:    
+        
+        if n in self.cache:
+            return self.cache[n]
+        
+        if n == 0 or n == 1:
+            return 1
+        
+        else:
+            result = self.climbStairs(n-1) + self.climbStairs(n-2)
+            self.cache[n] = result
+            return result
